c99696622d222395435838543b333750edf03ec7
league <maddoxseul@sbsd.net>
2025-09-27 13:00:05 -0700

    almost doen with level mod 2 just two more classes probably

--- src/_02_Intro_To_Searching_Algorithms/_03_SearchTest.java
+++ src/_02_Intro_To_Searching_Algorithms/_03_SearchTest.java
@@ -16,6 +16,7 @@ class _03_SearchTest {
     	int index1 = _01_LinearSearch.linearSearch(words, "hello");
     	int index2 = _01_LinearSearch.linearSearch(words, "hi");
     	int index3 = _01_LinearSearch.linearSearch(words, "konnichiwa");
+
     	
         // 1. Use the assertEquals() method to test your linear search method
     	System.out.println(index1);
@@ -28,6 +29,19 @@ class _03_SearchTest {
     public void testBinarySearch() {
         // 2. Use the assertEquals() method to test your binary search method
         //    remember that the array must be sorted
-        assertEquals(false, true);
+    	int[] array = {-8, 0, 1, 8, 387, 6739};
+    	int index1 = _02_BinarySearch.binarySearch(array, 0, array.length-1, -8);
+    	int index2 = _02_BinarySearch.binarySearch(array, 0, array.length-1,0);
+    	int index3 = _02_BinarySearch.binarySearch(array, 0, array.length-1, 1);
+    	int index4 = _02_BinarySearch.binarySearch(array, 0, array.length-1, 8);
+    	int index5 = _02_BinarySearch.binarySearch(array, 0, array.length-1,387);
+    	int index6 = _02_BinarySearch.binarySearch(array, 0, array.length-1,6739);
+    	System.out.println(index1 + ", "+ index2 + ", " + index3 + ", " + index4 + ", " + index5 + ", " + index6 + ", ");
+    	assertEquals(0, index1);
+    	assertEquals(1, index2);
+    	assertEquals(2, index3 );
+    	assertEquals(3, index4);
+    	assertEquals(4, index5);
+    	assertEquals(5, index6 );
     }
 }
--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -6,12 +6,63 @@ import java.util.List;
  * Write your methods in this class
  */
 public class Algorithms {
-    
-    public static String multiply(int num1, int num2) {
-        return null;
-    }
-    
-    public static int findBrokenEgg(List<String> eggs) {
-        return 0;
-    }
+
+	public static String multiply(int num1, int num2) {
+		return null;
+		
+	}
+
+	public static int findBrokenEgg(List<String> eggs) {
+		int crackedEgg;
+		for (int i = 0; i < eggs.size(); i++) {
+			if (eggs.get(i).contains("cracked")) {
+				return i;
+			}
+		}
+		return 0;
+
+	}
+	   public static int countPearls(List<Boolean> oysters){
+int num = 0;
+		for (boolean i:oysters) {
+			   if(i) {
+				   num++;
+			   }
+			}
+		return num;
+		   
+	   }
+
+	public static double findTallest(List<Double> peeps) {
+		// TODO Auto-generated method stub
+		double highest = 0;
+		for(int i  =0; i<peeps.size(); i++) {
+			if(peeps.get(i)>highest) {
+				highest = peeps.get(i);
+			}
+		}
+		return highest;
+	}
+
+	public static Object findLongestWord(List<String> words) {
+		// TODO Auto-generated method stub
+		String longest = "";
+		
+		for(String i:words) {
+			if(i.length()>longest.length()) {
+				longest = i;
+			}
+		}
+		return longest;
+	}
+
+	public static Boolean containsSOS(List<String> message) {
+		// TODO Auto-generated method stub
+		for(String i:message) {
+			if(i.contains("... --- ...")) {
+			return true;
+			}
+		}
+		return false;
+	}
 }
